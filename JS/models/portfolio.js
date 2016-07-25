4ad7a61f0576433697eb5c4391c29d5ffecbebe6
Derek Hewitt <derekmhewitt@gmail.com>
2016-07-25 09:23:10 -0700

    code reviewed, few minor edits

--- JS/models/portfolio.js
+++ JS/models/portfolio.js
@@ -13,7 +13,6 @@
     var template = Handlebars.compile($(scriptTemplateId).text());
     this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
     this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-    // this.body = marked(this.body);
     return template(this);
   };
 
--- JS/views/adminPage.js
+++ JS/views/adminPage.js
@@ -1,12 +1,13 @@
 'use strict';
-
-portfolioView.initAdminPage = function() {
-  var template = Handlebars.compile($('#author-template').html());
-
-  Article.numWordsByAuthor().forEach(function(stat) {
-    $('.author-stats').append(template(stat));
-  });
-  $('#portfolio-stats .articles').text(Project.allProjects.length);
-  $('#portfolio-stats .words').text(Project.numWordsAll());
-};
-Project.fetchAll(portfolioView.initAdminPage);
+(function(module) {
+  portfolioView.initAdminPage = function() {
+    // var template = Handlebars.compile($('#author-template').html());
+    //needs template on admin.html, will throw an error until that is fixed
+    Article.numWordsByAuthor().forEach(function(stat) {
+      $('.author-stats').append(template(stat));
+    });
+    $('#portfolio-stats .articles').text(Project.allProjects.length);
+    $('#portfolio-stats .words').text(Project.numWordsAll());
+  };
+  Project.fetchAll(portfolioView.initAdminPage);
+})(window);
